46366abf02a5669e7273fac2715b5e330dfc3902
Befikadu Tesfaye <132171539+0xfke@users.noreply.github.com>
2025-05-07 23:38:55 +0300

    Update appV1.py

--- appV1.py
+++ appV1.py
@@ -26,7 +26,7 @@ def calculate_entropy(data):
         return 0
     entropy = 0
     for x in range(256):
-        p_x = float(data.count(bytes([x]))) / len(data)  # Use bytes([x]) instead of chr(x)
+        p_x = float(data.count(bytes([x]))) / len(data)  
         if p_x > 0:
             entropy += - p_x * math.log(p_x, 2)
     return entropy
